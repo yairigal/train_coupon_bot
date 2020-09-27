636491146ab1ef17ca34c1a168efcf0b04c085b2
Yair Yigal <yigalyairn@gmail.com>
2020-09-27 17:37:58 +0300

    trying to abstract a state

--- bot.py
+++ bot.py
@@ -23,6 +23,17 @@ def log_user(handler_function):
     return wrapping_handler_function
 
 
+def reply_message(update, message, keyboard: List[List[str]] = None):
+    if keyboard is not None:
+        update.message.reply_text(message,
+                                  reply_markup=ReplyKeyboardMarkup(
+                                      keyboard=keyboard,
+                                      one_time_keyboard=True))
+
+    else:
+        update.message.reply_text(message, reply_markup=ReplyKeyboardRemove())
+
+
 class States:
     (ID,
      PHONE,
@@ -36,16 +47,147 @@ class States:
 
 
 class State(metaclass=ABCMeta):
-    def __init__(self, state_id):
-        self.state_id = state_id
+    STATE_NUMBER = NotImplemented
+
+    def __init__(self, bot):
+        self.bot = bot
 
     @abstractmethod
-    def pre_execute(self, update, context):
+    def pre_execute(self, update, context, *args, **kwargs):
         return NotImplemented
 
     @abstractmethod
-    def on_trigger(self, update, context):
+    def validate_input(self, user_input):
         return NotImplemented
+
+    @abstractmethod
+    def on_invalid_input(self, update, context, *args, **kwargs):
+        return NotImplemented
+
+    @abstractmethod
+    def on_trigger(self, update, context, *args, **kwargs):
+        return NotImplemented
+
+
+class IdState(State):
+    STATE_NUMBER = States.ID
+
+    def pre_execute(self, update, context, *args, **kwargs):
+        reply_message(update, message='Please enter your ID')
+
+    def validate_input(self, id_arg):
+        return re.fullmatch(r'\d+', id_arg) is not None
+
+    def on_invalid_input(self, update, context, *args, **kwargs):
+        reply_message(update, 'ID is not valid, please enter valid ID')
+
+    def on_trigger(self, update, context, *args, **kwargs):
+        context.user_data['id'] = args[0]
+
+
+class PhoneState(State):
+    STATE_NUMBER = States.PHONE
+
+    def pre_execute(self, update, context, *args, **kwargs):
+        reply_message(update, f'Success! ID is {args[0]}. Please enter your phone number')
+
+    def validate_input(self, phone):
+        return re.fullmatch(r'\d+', phone) is not None
+
+    def on_invalid_input(self, update, context, *args, **kwargs):
+        reply_message(update, 'phone number is not valid, please enter valid phone number')
+
+    def on_trigger(self, update, context, *args, **kwargs):
+        context.user_data['phone'] = args[0]
+
+
+class EmailState(State):
+    STATE_NUMBER = States.EMAIL
+
+    def pre_execute(self, update, context, *args, **kwargs):
+        phone = context.user_data['phone']
+        reply_message(update, f'Success! phone number is {phone}. Please enter your email address')
+
+    def validate_input(self, email):
+        return re.fullmatch(r'.+@.+', email) is not None
+
+    def on_invalid_input(self, update, context, *args, **kwargs):
+        reply_message(update, 'email is not valid, please enter valid email address')
+
+    def on_trigger(self, update, context, *args, **kwargs):
+        context.user_data['email'] = args[0]
+
+
+class OriginStationState(State):
+    STATE_NUMBER = States.HANDLE_ORIGIN_STATION
+
+    @property
+    def train_stations(self):
+        return self.bot.train_stations
+
+    def pre_execute(self, update, context, *args, **kwargs):
+        email = context.user_data['email']
+        reply_message(update,
+                      f'Success! email address is {email}. Please choose an origin station from the list below',
+                      keyboard=[[i] for i in self.train_stations])
+
+    def validate_input(self, origin_station):
+        return origin_station in self.train_stations
+
+    def on_invalid_input(self, update, context, *args, **kwargs):
+        reply_message(update,
+                      'Please choose a station from the list below',
+                      keyboard=[[i] for i in self.train_stations])
+
+    def on_trigger(self, update, context, *args, **kwargs):
+        context.user_data['origin_station_id'] = train_api.train_station_name_to_id(args[0])
+
+
+class DestinationStationState(State):
+    STATE_NUMBER = States.HANDLE_DEST_STATION
+
+    @property
+    def train_stations(self):
+        return self.bot.train_stations
+
+    def pre_execute(self, update, context, *args, **kwargs):
+        reply_message(update,
+                      f'Success! origin station picked is {args[0]}.\n'
+                      f'Please choose a destination station from the list below',
+                      keyboard=[[i] for i in self.train_stations])
+
+    def validate_input(self, destination_station):
+        return destination_station in self.train_stations
+
+    def on_invalid_input(self, update, context, *args, **kwargs):
+        reply_message(update,
+                      'Please choose a station from the list below',
+                      keyboard=[[i] for i in self.train_stations])
+
+    def on_trigger(self, update, context, *args, **kwargs):
+        context.user_data['dest_station_id'] = train_api.train_station_name_to_id(args[0])
+        for day in self.bot._next_week:
+            try:
+                trains = list(train_api.get_available_trains(origin_station_id=context.user_data['origin_station_id'],
+                                                             dest_station_id=context.user_data['dest_station_id'],
+                                                             date=day))
+                if len(trains) > 0:
+                    trains = {
+                        f"{self.bot._get_hour(train['DepartureTime'])} - {self.bot._get_hour(train['ArrivalTime'])}":
+                            train for train in trains}
+                    context.user_data['trains'] = trains
+                    context.user_data['date'] = day
+                    self.bot._reply_train_summary(update, context)
+                    return States.HANDLE_TRAIN
+
+            except (ValueError, AttributeError):
+                reply_message(update, 'An error occurred on the server, Please try again')
+                return self.handle_start(update, context)
+
+        else:
+            self.reply_message(update,
+                               "No trains are available for the next week, closing conversation")
+            return self.cancel(update, context)
 
 
 class TrainCouponBot:
@@ -61,6 +203,12 @@ class TrainCouponBot:
         logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                             level=logger_level)
         self.logger = logging.getLogger(__name__)
+
+        self.id_state = IdState(bot=self)
+        self.phone_state = PhoneState(bot=self)
+        self.email_state = EmailState(bot=self)
+        self.origin_station_state = OriginStationState(bot=self)
+        self.destination_station_state = DestinationStationState(bot=self)
 
     def run(self):
         # Create the EventHandler and pass it your bot's token.
@@ -168,135 +316,92 @@ class TrainCouponBot:
     def _reformat_to_readable_date(self, d):
         return re.fullmatch("(.*) \d+:.*", d.ctime()).group(1)
 
-    def _reply_message(self, update, message, keyboard: List[List[str]] = None):
-        if keyboard is not None:
-            update.message.reply_text(message,
-                                      reply_markup=ReplyKeyboardMarkup(
-                                          keyboard=keyboard,
-                                          one_time_keyboard=True))
-
-        else:
-            update.message.reply_text(message, reply_markup=ReplyKeyboardRemove())
-
     def _reply_train_summary(self, update, context):
         origin_station = train_api.train_station_id_to_name(context.user_data['origin_station_id'])
         dest_station = train_api.train_station_id_to_name(context.user_data['dest_station_id'])
         selected_date = self._reformat_to_readable_date(context.user_data['date'])
-        self._reply_message(update,
-                            f'Displaying trains for\n'
-                            f'{origin_station} -> {dest_station}\n'
-                            f'on {selected_date}',
-                            keyboard=[[i] for i in context.user_data['trains'].keys()])
+        self.reply_message(update,
+                           f'Displaying trains for\n'
+                           f'{origin_station} -> {dest_station}\n'
+                           f'on {selected_date}',
+                           keyboard=[[i] for i in context.user_data['trains'].keys()])
 
     # Handlers
     @log_user
     def handle_start(self, update, context):
         self._save_user(update.message.from_user)
-        update.message.reply_text('Please enter your ID', reply_markup=ReplyKeyboardRemove())
+        self.id_state.pre_execute(update, context)
         return States.ID
 
     @log_user
     def handle_id(self, update, context):
         user_id = update.message.text
-        if not self._id_valid(user_id):
-            self._reply_message(update, 'ID is not valid, please enter valid ID')
+        if not self.id_state.validate_input(user_id):
+            self.id_state.on_invalid_input(update, context)
             return States.ID
 
-        context.user_data['id'] = user_id
-        self._reply_message(update, f'Success! ID is {user_id}. Please enter your phone number')
+        self.id_state.on_trigger(update, context)
+        self.phone_state.pre_execute(update, context)
         return States.PHONE
 
     @log_user
     def handle_phone(self, update, context):
         phone = update.message.text
-        if not self._phone_valid(phone):
-            self._reply_message(update, 'phone number is not valid, please enter valid phone number')
+        if not self.phone_state.validate_input(phone):
+            self.phone_state.on_invalid_input(update, context)
             return States.PHONE
 
-        context.user_data['phone'] = phone
-        self._reply_message(update, f'Success! phone number is {phone}. Please enter your email address')
+        self.phone_state.on_trigger(update, context, phone)
+        self.email_state.pre_execute(update, context)
         return States.EMAIL
 
     @log_user
     def handle_email(self, update, context):
         email = update.message.text
-        if not self._email_valid(email):
-            self._reply_message(update, 'email is not valid, please enter valid email address')
+        if not self.email_state.validate_input(email):
+            self.email_state.on_invalid_input(update, context)
             return States.EMAIL
 
-        context.user_data['email'] = email
-        self._reply_message(update,
-                            f'Success! email address is {email}. Please choose an origin station from the list below',
-                            keyboard=[[i] for i in self.train_stations])
+        self.email_state.on_trigger(update, context, email)
+        self.origin_station_state.pre_execute(update, context)
         return States.HANDLE_ORIGIN_STATION
 
     @log_user
     def handle_origin_station(self, update, context):
         origin_station = update.message.text
-        if origin_station not in self.train_stations:
-            self._reply_message(update,
-                                'Please choose a station from the list below',
-                                keyboard=[[i] for i in self.train_stations])
+        if not self.origin_station_state.validate_input(origin_station):
+            self.origin_station_state.on_invalid_input(update, context)
             return States.HANDLE_ORIGIN_STATION
 
-        context.user_data['origin_station_id'] = train_api.train_station_name_to_id(origin_station)
-        self._reply_message(update,
-                            f'Success! origin station picked is {origin_station}.\n'
-                            f'Please choose a destination station from the list below',
-                            keyboard=[[i] for i in self.train_stations])
+        self.origin_station_state.on_trigger(update, context, origin_station)
+        self.destination_station_state.pre_execute(update, context, origin_station)
         return States.HANDLE_DEST_STATION
 
     @log_user
     @run_async
     def handle_dest_station(self, update, context):
         destination_station = update.message.text
-        if destination_station not in self.train_stations:
-            self._reply_message(update,
-                                'Please choose a station from the list below',
-                                keyboard=[[i] for i in self.train_stations])
+        if not self.destination_station_state.validate_input(destination_station):
+            self.destination_station_state.on_invalid_input(update, context)
             return States.HANDLE_DEST_STATION
 
-        context.user_data['dest_station_id'] = train_api.train_station_name_to_id(destination_station)
-
-        for day in self._next_week:
-            try:
-                trains = list(train_api.get_available_trains(origin_station_id=context.user_data['origin_station_id'],
-                                                             dest_station_id=context.user_data['dest_station_id'],
-                                                             date=day))
-                if len(trains) > 0:
-                    trains = {
-                        f"{self._get_hour(train['DepartureTime'])} - {self._get_hour(train['ArrivalTime'])}": train
-                        for train in trains}
-                    context.user_data['trains'] = trains
-                    context.user_data['date'] = day
-                    self._reply_train_summary(update, context)
-                    return States.HANDLE_TRAIN
-
-            except (ValueError, AttributeError):
-                self._reply_message(update,
-                                    'An error occurred on the server, Please try again')
-                return self.handle_start(update, context)
-
-        else:
-            self._reply_message(update,
-                                "No trains are available for the next week, closing conversation")
-            return self.cancel(update, context)
+        return self.destination_station_state.on_trigger(update, context, destination_station)
 
     @log_user
     @run_async
     def handle_train(self, update, context):
         train_date = update.message.text
         if train_date not in context.user_data['trains'].keys():
-            self._reply_message(update,
-                                'Please select a train from the list below',
-                                keyboard=[[i] for i in context.user_data['trains'].keys()])
+            self.reply_message(update,
+                               'Please select a train from the list below',
+                               keyboard=[[i] for i in context.user_data['trains'].keys()])
             return States.HANDLE_TRAIN
 
         current_train = context.user_data['trains'][train_date]
 
         image_path = 'image.jpeg'
         try:
-            self._reply_message(update, "Ordering coupon...")
+            self.reply_message(update, "Ordering coupon...")
             train_api.request_train(user_id=context.user_data['id'],
                                     mobile=context.user_data['phone'],
                                     email=context.user_data['email'],
@@ -305,16 +410,16 @@ class TrainCouponBot:
 
         except AttributeError:
             # error with the arguments passed
-            self._reply_message(update,
-                                'Error occurred in the server, some details might be wrong, please enter them again')
+            self.reply_message(update,
+                               'Error occurred in the server, some details might be wrong, please enter them again')
             return self.handle_start(update, context)
 
         except (ValueError, RuntimeError) as e:
             # No bardcode image found
             self.logger.error(f'no barcode image received, error={e}')
-            self._reply_message(update,
-                                'No barcode image received from the server. This might happen if the same seat is '
-                                'ordered twice. Please pick another seat')
+            self.reply_message(update,
+                               'No barcode image received from the server. This might happen if the same seat is '
+                               'ordered twice. Please pick another seat')
             self._reply_train_summary(update, context)
             return States.HANDLE_TRAIN
 
@@ -323,9 +428,9 @@ class TrainCouponBot:
                                                 action=ChatAction.UPLOAD_PHOTO)
             update.message.reply_photo(f)
 
-        self._reply_message(update,
-                            'Get another coupon?',
-                            keyboard=[['Order Different Train'], ['Order the same', 'Close']])
+        self.reply_message(update,
+                           'Get another coupon?',
+                           keyboard=[['Order Different Train'], ['Order the same', 'Close']])
 
         return States.WHETHER_TO_CONTINUE
 
@@ -333,15 +438,15 @@ class TrainCouponBot:
     def handle_whether_to_continue(self, update, context):
         answer = update.message.text
         if answer not in ['Order Different Train', 'Order the same', 'Close']:
-            self._reply_message(update,
-                                'Please choose a valid option',
-                                keyboard=[['Order Different Train'], ['Order the same', 'Close']])
+            self.reply_message(update,
+                               'Please choose a valid option',
+                               keyboard=[['Order Different Train'], ['Order the same', 'Close']])
             return States.WHETHER_TO_CONTINUE
 
         if answer == 'Order Different Train':
-            self._reply_message(update,
-                                'Choose origin station',
-                                keyboard=[[i] for i in self.train_stations])
+            self.reply_message(update,
+                               'Choose origin station',
+                               keyboard=[[i] for i in self.train_stations])
             return States.HANDLE_ORIGIN_STATION
 
         elif answer == 'Order the same':
