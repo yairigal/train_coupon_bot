ba005c9836d8dccedceabb34e54f4183eeb89f7d
Yair Yigal <yigalyairn@gmail.com>
2020-09-27 11:44:14 +0300

    now handeling the expcetion from the api

--- bot.py
+++ bot.py
@@ -263,9 +263,14 @@ class TrainCouponBot:
             return States.HANDLE_DATE
 
         date = context.user_data['dates'][update.message.text]
-        res = train_api.get_available_trains(origin_station_id=context.user_data['origin_station_id'],
-                                             dest_station_id=context.user_data['dest_station_id'],
-                                             date=date)
+        try:
+            res = train_api.get_available_trains(origin_station_id=context.user_data['origin_station_id'],
+                                                 dest_station_id=context.user_data['dest_station_id'],
+                                                 date=date)
+        except (ValueError, AttributeError):
+            self._reply_message(update,
+                                'An error occurred on the server, Please retry again')
+            return self.handle_start(update, context)
 
         trains = {f"{train['DepartureTime']} - {train['ArrivalTime']}": train for train in res}
         if len(trains) == 0:
@@ -291,11 +296,31 @@ class TrainCouponBot:
         current_train = context.user_data['trains'][train_date]
 
         image_path = 'image.jpeg'
-        train_api.request_train(user_id=context.user_data['id'],
-                                mobile=context.user_data['phone'],
-                                email=context.user_data['email'],
-                                train_json=current_train,
-                                image_dest=image_path)
+        try:
+            train_api.request_train(user_id=context.user_data['id'],
+                                    mobile=context.user_data['phone'],
+                                    email=context.user_data['email'],
+                                    train_json=current_train,
+                                    image_dest=image_path)
+
+        except AttributeError:
+            # error with the arguments passed
+            self._reply_message(update,
+                                'Error occurred in the server, some details might be wrong, please enter them again')
+            self._reply_message(update,
+                                "Please enter your ID")
+            return States.ID
+
+        except (ValueError, RuntimeError) as e:
+            # No bardcode image found
+            self.logger.error(f'no barcode image received, error={e}')
+            self._reply_message(update,
+                                'No barcode image received from the server. This might happen if the same seat is '
+                                'ordered twice. Please pick another seat')
+            self._reply_message(update,
+                                'Choose a day',
+                                keyboard=[[i] for i in context.user_data['dates'].keys()])
+            return States.HANDLE_DATE
 
         with open(image_path, 'rb') as f:
             update.message.bot.send_chat_action(chat_id=update.effective_message.chat_id,
